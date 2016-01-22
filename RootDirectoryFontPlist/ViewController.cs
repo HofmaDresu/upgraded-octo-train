e8e45b3c44d49cec596c295f7997a332e06c9548
Matt Hoffman <hofmadresu@gmail.com>
2016-01-22 10:39:19 -0500

    Initial commit. Create working solution with font in root

---
+++ RootDirectoryFontPlist/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace RootDirectoryFontPlist
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var button = new UIButton(new CGRect(100, 100, 100, 100));
+            button.SetTitle('\ue6fe'.ToString(), UIControlState.Normal);
+            button.Font = UIFont.FromName("icomoon", (nfloat)24);
+            button.BackgroundColor = UIColor.Clear;
+            button.SetTitleColor(UIColor.Green, UIControlState.Normal);
+            View.AddSubview(button);
+        }
+    }
+}
